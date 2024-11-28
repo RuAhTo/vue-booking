1ca28c5a44d010310c672279d440f2b083c44894
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-11-28 17:20:08 +0100

    homepage + navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Navbar',
+    setup(){
+
+    }
+})
+</script>
+
+<template>
+    <nav>
+        <div class="nav-items">
+            <h2>Klipp & Klang</h2>
+        </div>
+        <div class="nav-items">
+            <a href="/booking">Bokningar</a>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+    nav{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: var(--primary-color);
+    }
+.nav-items{
+    padding: 0 2rem;
+}
+
+</style>
--- src/view/HomePage.vue
+++ src/view/HomePage.vue
@@ -1,19 +1,51 @@
 <script lang="ts">
+import Navbar from '../components/Navbar.vue';
     export default{
-        name: 'HomePage'
+        name: 'HomePage',
+        components: {
+            Navbar,
+        }
     }
 </script>
 
 <template>
     <header>
-        <div>
-            <h2>Klipp & Klang</h2>
-        </div>
-        <nav class="navbar">
-        </nav>
+        <Navbar/>
     </header>
     <main>
-        <a class="btn book-btn" href="/booking">Boka här</a>
-
+        <div class="hero">
+            <a class="book-btn" href="/booking">Boka här</a>
+        </div>
     </main> 
 </template>
+
+<style scoped>
+.hero {
+    width: 100%;
+    background-image: url('/hero-klipp.jpg'); 
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.book-btn{
+    background-color: var(--primary-color);
+    text-decoration: none;
+    color: black;
+    padding: 2rem;
+    width: 8rem;
+    text-align: center;
+    border-radius: 1rem;
+    font-weight: 100;
+    font-size: x-large;
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+}
+
+.book-btn:hover{
+    
+}
+
+</style>
